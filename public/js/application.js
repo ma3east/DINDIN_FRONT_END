b9a8bc28e48da5650238bd8f218bd769542fa057
fran-k <franziskaklingner@hotmail.com>
2015-08-26 12:23:27 +0100

    transactions can be cancelled

--- public/js/application.js
+++ public/js/application.js
@@ -99,20 +99,22 @@ $(".rate_transaction").on("submit", function(){
   event.preventDefault();
   var rating = $(this).find("select").val();
   var transaction_id = $(this).attr("id");
-  var giver_id = $(this).data().giverId;
-  console.log(giver_id);
-  var status = "rated";
-  // Reset status and rating if the transaction failed
+  var user_id = $(this).data().userid;
+  var status = $(this).data().status;
+  if (status === "taken") {
+    status = "ratedByOne";
+  } else {
+    status = "closed";
+  }
   $.ajax({
-    url: "http://localhost:9000/api/users/" + document.cookie.split(";")[1].split("=")[1],
+    url: "http://localhost:9000/api/users/" + user_id,
     type: "PUT",
-    headers: {
-      'Authorization': "Bearer " + document.cookie.split(";")[0].split("=")[1]
-    },
+    headers: { 'Authorization': "Bearer " + document.cookie.split(";")[0].split("=")[1] },
     contentType: "application/json",
     data: JSON.stringify({$inc: {reputation: rating}})
   })
   .done(function(){
+  // Reset status and rating if the transaction failed
       if(rating == -5) {
         status="open";
         rating=0;
@@ -127,5 +129,28 @@ $(".rate_transaction").on("submit", function(){
     })
   })
 })
+
+$(".cancel_transaction").on("click", function(){
+  event.preventDefault();
+  var transaction_id = $(this).attr("id");
+  var user_id = $(this).data().userid;
+  $.ajax({
+      url: "http://localhost:9000/api/transactions/" + transaction_id,
+      type: "PUT",
+      beforeSend: function(xhr){xhr.setRequestHeader('Authorization', "Bearer " + document.cookie.split(";")[0].split("=")[1]);},
+      data: {status: "open"}
+    }).done(function(){
+  $.ajax({
+    url: "http://localhost:9000/api/users/" + user_id,
+    type: "PUT",
+    headers: { 'Authorization': "Bearer " + document.cookie.split(";")[0].split("=")[1] },
+    contentType: "application/json",
+    data: JSON.stringify({$inc: {reputation: -2}})
+  }).done(function(){
+      window.location.href="http://localhost:3000";
+    })
+  })
+})
+
 
 });
